6f7bceecf28ba92447ce92cae599528c08684ba5
golap-raj <golapraj47@gmail.com>
2025-02-08 14:00:08 -0600

    resolve

---
+++ reverce.js
@@ -0,0 +1,7 @@
+const senten = 'my name is a golap';
+let reverse = '';
+for(let letter of senten){
+    reverse = letter + reverse;
+    
+}
+console.log(reverse)
--- splite.js
+++ splite.js
@@ -1,6 +1,27 @@
 // const read = 'i read in a book';
 // console.log(read.split(' '))
 
-const person = "Golap is a good boy";
+// const person = "Golap is a good boy";
+// // console.log(person.split(' '))
 // console.log(person.split(' '))
-console.log(person.split(' '))
+
+// const friends = 'rahim, fahim, basar, golap'
+// console.log(friends.split(' ,'))
+
+// const friends = [ 'golap', 'raj', 'tahmid', 'hasan' ];
+// // console.log(friends.split(' '))
+// console.log(friends.join('-'))
+
+
+
+// const address = 'bangladesh';
+// // console.log(address.slice(0, 3))
+// // console.log(address.split(' '))
+// const frend = ['basar', 'golap']
+// console.log(frend.join('|'))
+
+const first = 'Golap';
+const last = 'Raj';
+const fullname = first.concat(' ').concat(last)
+
+console.log(fullname)
