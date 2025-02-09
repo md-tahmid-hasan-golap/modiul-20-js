deaae48d2dfbe3c855ed4a7a4494b645f9b5a973
golap-raj <golapraj47@gmail.com>
2025-02-09 10:02:10 -0600

    java

---
+++ object.js
@@ -0,0 +1,17 @@
+// object diclaration niyom
+const persone = {
+    name : "Golap",
+    age : 20,
+    wight : 55,
+    isWebDeveloper : true,
+    language : "javaScript",
+    marrid : false,
+    "my jurnay" : ["rajshahi","dhaka"]
+}
+// persone.isWebDeveloper = false;
+// persone["isWebDeveloper"] = true
+// console.log(persone);
+// console.log(persone.age)
+// console.log(persone["isWebDeveloper"])
+console.log(Object.keys(persone));
+console.log(Object.values(persone))
--- reverce.js
+++ reverce.js
@@ -1,7 +1,21 @@
-const senten = 'my name is a golap';
-let reverse = '';
-for(let letter of senten){
-    reverse = letter + reverse;
+// const senten = 'my name is a golap';
+// let reverse = '';
+// for(let letter of senten){
+//     reverse = letter + reverse;
     
+// }
+// console.log(reverse)
+// let reverce = '';
+// const sectence = 'golap is a bad boy';
+// for(let sectences of sectence){
+//     // console.log(sectences)
+//     reverce = sectences + reverce;
+// }
+// console.log(reverce)
+let reverce = '';
+const senten = 'my mother is a house wife';
+for(let letter of senten){
+    // console.log(letter)
+    reverce = letter + reverce
 }
-console.log(reverse)
+console.log(reverce)
