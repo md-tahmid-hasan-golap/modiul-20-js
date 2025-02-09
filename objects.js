e92f970b10203471186944cd91c99f9824023f39
golap-raj <golapraj47@gmail.com>
2025-02-09 10:46:33 -0600

    add java code

---
+++ objects.js
@@ -0,0 +1,28 @@
+// const  persone = {
+//     name : "tahmid",
+//     school : "alinagar school & college",
+//     FatherName : "abdullah",
+//     motherName : "chemely",
+//     marrid : false,
+// }
+// for(let person in persone){
+//     console.log(person);
+//     console.log(persone[person])
+// }
+
+// persone.marrid = true
+// persone["school"] = "rajshahi"
+// console.log(persone)
+// console.log(Object.values(persone))
+
+const laptop ={
+    brand : "Hp",
+    price : 50000,
+    unick : true,
+    user : "golap"
+}
+// console.log(laptop)
+for(let laptops in laptop){
+    console.log(laptops);
+    console.log(laptop[laptops])
+}
---
+++ reverces.js
@@ -0,0 +1,9 @@
+const sentence = 'golap is a good boy'
+// console.log(sentence)
+let reverce = '';
+for(let letter of sentence){
+    console.log(letter)
+    reverce = letter + reverce;
+   
+}
+console.log(reverce)
